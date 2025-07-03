aead7edbb480b2749b7dce6f7218e594196d11bb
Harilyn <120591985+Harilyn@users.noreply.github.com>
2025-07-03 23:14:32 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,8 +22,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.png" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/favicon.png" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
